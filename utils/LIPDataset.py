962a8aad0eec144a4a557ee3868a8db2de637db7
nik1806 <nikhil.paliwal14@gmail.com>
2020-08-07 12:57:13 +0530

    Add: LIPDataset visualization function

--- utils/LIPDataset.py
+++ utils/LIPDataset.py
@@ -4,6 +4,44 @@ import pandas as pd
 import os
 import cv2
 
+def plot_data(sample):
+    ''' Drawing joints and bones on images '''
+
+    img = sample['image'] # retrive image
+    rec = sample['keypoints'] # retrive points coordinates
+
+    bombs = [[0,1],[1,2]
+            ,[3,4],[4,5]
+            ,[6,7],[7,8],[8,9]
+            ,[10,11],[11,12]
+            ,[13,14],[14,15] ]
+    colors = [(255,0,0),(255,0,0),
+              (0,255,0),(0,255,0),
+              (0,0,255),(0,0,255),(0,0,255),
+              (128,128,0),(128,128,0),
+              (128,0,128),(128,0,128)]
+
+    for b_id in range(len(bombs)):
+        b = bombs[b_id]
+        color = colors[b_id]
+        x1 = rec[ b[0] * 2 + 0]
+        y1 = rec[ b[0] * 2 + 1]
+
+        x2 = rec[ b[1] * 2 + 0]
+        y2 = rec[ b[1] * 2 + 1]
+
+        if x1 > 0 and x2 > 0 and y1 > 0 and y2 > 0:
+            img = cv2.line(img, (int(x1),int(y1)), (int(x2),int(y2)), color, 3) 
+        elif x1 > 0 and y1 > 0:
+            img = cv2.circle(img, (int(x1), int(y1)), 4, color, 4) 
+        elif x2 > 0 and y2 > 0:
+            img = cv2.circle(img, (int(x2), int(y2)), 4, color, 4)
+    
+    cv2.imshow('Human keypoints', img)
+    cv2.waitKey(0)
+        
+    cv2.destroyAllWindows()
+
 class LIPDataset(Dataset):
     ''' Custom dataset class to handle LIP data '''
 
