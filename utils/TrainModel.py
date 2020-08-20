ad67fdddb67d2a5dccefbc97ee5eb9ee2dce09bd
nik1806 <nikhil.paliwal14@gmail.com>
2020-08-20 11:27:55 +0530

    Add: TrainModel class - flexible training

--- utils/TrainModel.py
+++ utils/TrainModel.py
@@ -7,6 +7,112 @@ from torchvision import models
 import numpy as np
 from utils.LIPDataset import untransform_n_display
 
+class TrainModel(object):
+    ''' A class for performing training loops over datasets '''
+    def __init__(self, model, train_loader, valid_loader, criterion, optimizer):
+        '''
+            Args:
+                model: NN
+                train_loader, valid_loader: Data loaders
+                criterion: Loss function (Just class, not instance)
+                optimizer: Model weight optimizer (Just class)
+        '''
+        self.model = model
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
+        self.criterion = criterion() # creating instance here
+        self.optimizer_cls = optimizer
+        self.lr = 0.0 # dummy value
+
+    def __call__(self, epoch: int, lr: float = 0.0003, eval_freq: int = 20, save_model: bool = False):
+        '''
+        Perform forward, backward pass and weight update. Evaluate performance on 
+        validation data based on value of value of variabel 'eval_freq' per epoch.
+        Optionally save model based on condition of decrease in model loss.
+        Args: 
+            epoch: Number of training loop over whole training data
+            lr: Learning rate for training
+            eval_freq: Frequency of performing and printing - train and validation loss per epoch
+            save_model: If True and valid loss decrease, save model stats after every evaluation of performance on validation data 
+        Return: 
+            train_loss (list): Contain record of values of loss on training data
+            valid_loss (list): Contain record of values of loss on validation data
+        '''
+        assert(isinstance(epoch, int), 'The variable should be an int datatype')
+        # OPTIMIZER
+        if self.lr != lr: # new learning rate
+            self.lr = lr
+            # initialize new optimizer (since lr is changed)
+            self.optimizer = self.optimizer_cls(self.model.fc.parameters(), lr=self.lr)
+
+        # storing loss over whole training
+        train_loss = list()
+        valid_loss = list()
+        # perfrom evaluation and print results ''eval_freq' times per epoch
+        num_batches = len(self.train_loader)
+        prt_idx = num_batches // eval_freq
+
+        # prepare the net for training (autograd, dropout -> on)
+        self.model = self.model.cuda()
+        self.model.train()
+
+        for i in range(epoch): # looping over whole dataset
+            running_loss = 0.0
+            # training on batches of data
+            for batch_i, samples in enumerate(self.train_loader):
+                # get images and keypoints
+                images, keypts = samples['image'].cuda(), samples['keypoints'].cuda()
+                # forward pass
+                pred = self.model.forward(images)
+                # compute loss
+                loss = self.criterion(pred, keypts)
+                # zero the accumulate weight gradients
+                self.optimizer.zero_grad()
+                # backward pass (calculate current weight gradients)
+                loss.backward()
+                # update the weights
+                self.optimizer.step()
+
+                running_loss += loss.item() # storing loss
+
+                # Checking performance on validation data
+                # Print all the losses (frequency  is based on prt_freq value)
+                if batch_i % prt_idx == 0: 
+
+                    temp_loss = 0.0
+                    with torch.no_grad(): # switching off autograd engine, save memory and computation
+                        self.model.eval() # switching off dropout and batchnorm  
+                        for samples in self.valid_loader: # looping over batches
+                            # get images and keypoints
+                            images, keypts = samples['image'].cuda(), samples['keypoints'].cuda()
+                            # forward pass
+                            pred = self.model.forward(images)
+                            # compute loss
+                            loss = self.criterion(pred, keypts)
+                            
+                            temp_loss += loss.item()
+
+                    avg_train_loss = running_loss/prt_idx
+                    avg_valid_loss = temp_loss/len(self.valid_loader)
+                    print(f'Epoch: {i}, Batch: {batch_i}, Train Avg. Loss: {avg_train_loss}, Validation Avg. Loss:{avg_valid_loss}')
+                    # storing avg loss for analysis later
+                    train_loss.append(avg_train_loss)
+                    valid_loss.append(avg_valid_loss)
+                    running_loss = 0.0
+                    self.model.train() # return to trian mode for next loop
+
+                    break # break after one turn
+
+        print("Training complete")
+        # transfer to cpu
+        self.model = self.model.cpu()
+        torch.cuda.empty_cache()
+
+        return train_loss, valid_loss
+
+        
+
+
 def random_test(dataset, model, norm_param: list, display: bool = True):
     ''' Chose a random data point, perform inference. If set the 'display' flag display the results. 
     Args:
@@ -99,3 +205,5 @@ def set_requires_grad(model, value: bool = False):
     '''
     for param in model.parameters():
         param.set_requires_grad = value
+
+
